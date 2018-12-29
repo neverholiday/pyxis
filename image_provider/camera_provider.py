a9eac0c1b7548a8b9ba555d2568d5ea1042a7a09
neverholiday <hy.nasrun@gmail.com>
2018-12-29 14:01:09 +0700

    [COLOR_CALIBRATOR_V0] add camera provider for open image from camera

--- image_provider/camera_provider.py
+++ image_provider/camera_provider.py
@@ -17,10 +17,17 @@ import os
 #	LOCAL IMPORTS
 #
 
+import cv2
+import numpy as np
+
 ########################################################
 #
 #	GLOBALS
 #
+
+#   FOR TESTING
+CAMERA_DEVICE_WEBCAM = '/dev/video0'
+CAMERA_DEVICE_EXTERNAL = '/dev/video1'
 
 ########################################################
 #
@@ -37,4 +44,64 @@ import os
 #	CLASS DEFINITIONS
 #
 
+class Camera( object ):
 
+    def __init__( self, cameraDevice ):
+
+        #   get camera ID
+        cameraID = self.changeCameraDeviceToCameraID( cameraDevice )
+
+        #   initial camera capture
+        self.cameraCapture = cv2.VideoCapture( cameraID )
+        self.cameraCapture.set( cv2.CAP_PROP_FRAME_WIDTH, 640 )
+        self.cameraCapture.set( cv2.CAP_PROP_FRAME_HEIGHT, 480 )
+
+        #   initial frame and camera retrive
+        self.frame = None
+        self.retrive = None
+
+    def changeCameraDeviceToCameraID( self, cameraDevice ):
+        """
+            Checking camera device
+        """
+
+        if cameraDevice == '/dev/video0':
+            return 0
+
+        elif cameraDevice == '/dev/video1':
+            return 1
+        
+        else:
+            raise TypeError( "{} is not valid".format( cameraDevice ) )
+        
+
+    def read( self ):
+
+        #   READ!!!!        
+        self.retrive, self.frame = self.cameraCapture.read()
+
+        #   convert to RGB
+        self.frame = cv2.cvtColor( self.frame, cv2.COLOR_BGR2RGB )
+    
+    def end( self ):
+
+        #   release
+        self.cameraCapture.release()
+
+if __name__ == "__main__":
+    
+    #   initial camera capture
+    camera = Camera( CAMERA_DEVICE_WEBCAM )
+
+    while True:
+
+        camera.read()
+
+        cv2.imshow( "frame", camera.frame )
+
+        k = cv2.waitKey( 1 )
+        if k == ord( 'q' ):
+            break
+
+    camera.end()
+    cv2.destroyAllWindows()
