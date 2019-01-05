529a5ea7cae2dab1b36c13388ba60ebb0439174d
neverholiday <hy.nasrun@gmail.com>
2019-01-05 22:10:59 +0700

    [COLOR_CALIB_V0] add subapps of color calibrate when use camera

---
+++ color_calibrate_subapp.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2018  FIBO/KMUTT
+#			Written by Nasrun Hayeeyama
+#
+
+########################################################
+#
+#	STANDARD IMPORTS
+#
+
+import sys
+import os
+
+########################################################
+#
+#	LOCAL IMPORTS
+#
+
+from PyQt4 import QtCore
+from PyQt4 import QtGui
+
+from widget.color_calibrate_button_widget import ColorButton
+from widget.frame_widget import FrameWidget
+from config_generator import getConfigDict, saveConfig
+
+########################################################
+#
+#	GLOBALS
+#
+
+########################################################
+#
+#	EXCEPTION DEFINITIONS
+#
+
+########################################################
+#
+#	HELPER FUNCTIONS
+#
+
+########################################################
+#
+#	CLASS DEFINITIONS
+#
+
+class ColorCalibrateSubapp( QtGui.QWidget ):
+
+    def __init__( self, cameraDevice, configPathStr = None, savePath = '/tmpfs/config.ini' ):
+
+        #   SUPAAAAA
+        super( ColorCalibrateSubapp, self ).__init__()
+
+        #   get config dict structure
+        self.configDict = getConfigDict( path = configPathStr )
+        self.savePathStr = savePath
+
+        #   create color button and slider
+        self.colorButton = ColorButton( self.configDict )
+        self.frameCameraWidget = FrameWidget( cameraDevice, self.colorButton )
+
+        #   create export button
+        self.exportButton = QtGui.QPushButton( 'Export' )
+
+        #   connect export to callback function
+        self.exportButton.clicked.connect( self.exportButtonCallbackFunction )
+
+        #   create vertical box layout
+        self.verticalBoxLayout = QtGui.QVBoxLayout()
+
+        #   add widget 
+        self.verticalBoxLayout.addWidget( self.frameCameraWidget )
+        self.verticalBoxLayout.addWidget( self.colorButton )
+        self.verticalBoxLayout.addWidget( self.exportButton )
+
+        #   set to layout
+        self.setLayout( self.verticalBoxLayout )
+
+    def exportButtonCallbackFunction( self ):
+        
+        print "Save!"
+
+        saveConfig( self.configDict, savePathStr = self.savePathStr )
+
+if __name__ == "__main__":
+	
+	#	initial app
+	app = QtGui.QApplication( sys.argv )
+
+	#	call widget
+	widget = ColorCalibrateSubapp( '/dev/video1' )
+
+	#	show
+	widget.show()
+
+	#	execute app
+	sys.exit( app.exec_() )
---
+++ test_color_subapp.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2018  FIBO/KMUTT
+#			Written by Nasrun Hayeeyama
+#
+
+VERSIONNUMBER = 'v1.0'
+PROGRAM_DESCRIPTION = "one of pyxis, color calibrator"
+
+########################################################
+#
+#	STANDARD IMPORTS
+#
+
+import sys
+import os
+
+import optparse
+
+
+########################################################
+#
+#	LOCAL IMPORTS
+#
+
+from PyQt4 import QtCore
+from PyQt4 import QtGui
+from color_calibrate_subapp import ColorCalibrateSubapp
+
+########################################################
+#
+#	Standard globals
+#
+NUM_REQUIRE_ARGUMENT = 1
+
+########################################################
+#
+#	Program specific globals
+#
+
+########################################################
+#
+#	Helper functions
+#
+
+########################################################
+#
+#	Class definitions
+#
+
+########################################################
+#
+#	Function bodies
+#
+
+########################################################
+#
+#	main
+#	
+def main():
+	
+	#	define usage of programing
+	programUsage = "python %prog arg [option] " + str( VERSIONNUMBER ) + ', Copyright (C) 2018 FIBO/KMUTT'
+
+	#	initial parser instance
+	parser = optparse.OptionParser( usage = programUsage, description=PROGRAM_DESCRIPTION )
+
+	#	add option of main script
+
+	parser.add_option( "-i", "--importConfigPathStr", dest = "importConfigPathStr", type = "string", action = "store",
+						help = "Import config path." )
+	
+	parser.add_option( "-o", "--exportConfigPathStr", dest = "exportConfigPathStr", type = "string", action = "store",
+						help = "Export config path.", default = "/tmpfs/config.ini" )
+	
+
+	#	add option
+	( options, args ) = parser.parse_args()
+
+	#	check number of argument from NUM_REQUIRE_ARGUMENT
+	if len( args ) != NUM_REQUIRE_ARGUMENT:	
+		
+		#	raise error from parser
+		parser.error( "require {} argument(s)".format( NUM_REQUIRE_ARGUMENT ) )
+	
+
+	#########################################################
+	#
+	#		get option and argument
+	#
+	cameraDevice = args[ 0 ]
+	
+	importConfigPathStr = options.importConfigPathStr
+	exportConfigPathStr = options.exportConfigPathStr
+
+	#	initial app
+	app = QtGui.QApplication( sys.argv )
+
+	#	call widget
+	widget = ColorCalibrateSubapp( cameraDevice, configPathStr = importConfigPathStr, savePath = exportConfigPathStr )
+
+	#	show
+	widget.show()
+
+	#	execute app
+	sys.exit( app.exec_() )
+	
+
+########################################################
+#
+#	call main
+#
+
+if __name__=='__main__':
+	main()
+
